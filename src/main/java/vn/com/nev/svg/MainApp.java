6926eb15d883534aac1b7c5a332b5c08dc8ecae6
Khoi Bui Van <khoibv@nissho-vn.com>
2020-05-19 18:57:05 +0700

    Fix for draw3.svg

--- src/main/java/vn/com/nev/svg/MainApp.java
+++ src/main/java/vn/com/nev/svg/MainApp.java
@@ -1,76 +1,48 @@
 package vn.com.nev.svg;
 
-import java.io.File;
+import com.itextpdf.text.DocumentException;
 import java.io.IOException;
 import org.apache.batik.transcoder.TranscoderException;
-import org.apache.pdfbox.cos.COSName;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
-import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
-import org.apache.pdfbox.pdmodel.graphics.state.PDExtendedGraphicsState;
 
 public class MainApp {
 
+
+  private static final String INPUT_PDF = "input/svg1.pdf";
+//  private static final String INPUT_SVG = "input/sample.svg";
+    private static final String INPUT_SVG = "input/draw.svg";
+  private static final String OUTPUT_PDF = "output/svgpdf.pdf";
 
   /**
    * This will load a PDF document and add a single image on it.
    *
    * @param args Command line arguments.
    */
-  public static void main(String[] args) throws IOException, TranscoderException {
+  public static void main(String[] args) {
     MainApp app = new MainApp();
 
-    final String inputPdf = "input/svg1.pdf";
-    final String inputSvg = "input/sample.svg";
-//    final String inputSvg = "input/draw3.svg";
-    final String outputPdf = "output/svgpdf.pdf";
-
-    app.createPDFFromImage(inputPdf, inputSvg, outputPdf);
+//    app.test01();
+    app.test02();
   }
 
-  /**
-   * Add an image to an existing PDF document.
-   *
-   * @param inputFile  The input PDF to add the image to.
-   * @param imagePath  The filename of the image to put in the PDF.
-   * @param outputFile The file to write to the pdf to.
-   * @throws IOException If there is an error writing the data.
-   */
-  public void createPDFFromImage(String inputFile, String imagePath, String outputFile)
-      throws IOException, TranscoderException {
-    try (PDDocument doc = PDDocument.load(new File(inputFile))) {
-      //we will add the image to the first page.
-      PDPage page = doc.getPage(0);
-
-      // createFromFile is the easiest way with an image file
-      // if you already have the image in a BufferedImage,
-      // call LosslessFactory.createFromImage() instead
-//      PDImageXObject pdImage = PDImageXObject.createFromFile(imagePath, doc);
-      PDImageXObject pdImage = Utility.createImageObjectFromSvg(imagePath, doc);
-
-      try (PDPageContentStream contentStream = new PDPageContentStream(doc, page, AppendMode.APPEND,
-          true, true)) {
-        contentStream.saveGraphicsState();
-        PDExtendedGraphicsState pdExtGfxState = new PDExtendedGraphicsState();
-        pdExtGfxState.getCOSObject().setItem(COSName.BM, COSName.MULTIPLY); // pdExtGfxState.setBlendMode(BlendMode.MULTIPLY) doesn't work yet, maybe in later version
-        pdExtGfxState.setNonStrokingAlphaConstant(1f);
-        contentStream.setGraphicsStateParameters(pdExtGfxState);
-
-        // contentStream.drawImage(ximage, 20, 20 );
-        // better method inspired by http://stackoverflow.com/a/22318681/535646
-        // reduce this value if the image is too large
-
-
-        float scale = 1f;
-        contentStream
-            .drawImage(pdImage, 20, 20, pdImage.getWidth() * scale, pdImage.getHeight() * scale);
-
-        contentStream.restoreGraphicsState();
-      }
-      doc.save(outputFile);
+  private void test02() {
+    // Using PdfBox + batik
+    PdfBoxBatikCreator creator = new PdfBoxBatikCreator();
+    try {
+      creator.createPDF(INPUT_PDF, INPUT_SVG, "output/test02.pdf");
+    } catch (IOException | TranscoderException e) {
+      e.printStackTrace();
     }
   }
+
+  private void test01() {
+    // Using iText
+    ITextPdfCreator creator = new ITextPdfCreator();
+    try {
+      creator.createPdf(INPUT_SVG, "output/test01.pdf");
+    } catch (IOException | DocumentException e) {
+      e.printStackTrace();
+    }
+  }
+
 
 }
---
+++ src/main/java/vn/com/nev/svg/PdfBoxBatikCreator.java
@@ -0,0 +1,98 @@
+package vn.com.nev.svg;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import org.apache.batik.transcoder.SVGAbstractTranscoder;
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
+import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.pdmodel.graphics.state.PDExtendedGraphicsState;
+
+public class PdfBoxBatikCreator {
+
+  /**
+   * Add an image to an existing PDF document.
+   *
+   * @param inputPdf  The input PDF to add the image to.
+   * @param inputSvg  The filename of the image to put in the PDF.
+   * @param outputPdf The file to write to the pdf to.
+   * @throws IOException If there is an error writing the data.
+   */
+  public void createPDF(String inputPdf, String inputSvg, String outputPdf)
+      throws IOException, TranscoderException {
+    try (PDDocument doc = PDDocument.load(new File(inputPdf))) {
+      //we will add the image to the first page.
+      PDPage page = doc.getPage(0);
+
+      // createFromFile is the easiest way with an image file
+      // if you already have the image in a BufferedImage,
+      // call LosslessFactory.createFromImage() instead
+//      PDImageXObject pdImage = PDImageXObject.createFromFile(imagePath, doc);
+      PDImageXObject pdImage = createImageObjectFromSvg(inputSvg, doc);
+
+      try (PDPageContentStream contentStream = new PDPageContentStream(doc, page, AppendMode.APPEND,
+          true, true)) {
+        contentStream.saveGraphicsState();
+        PDExtendedGraphicsState pdExtGfxState = new PDExtendedGraphicsState();
+        pdExtGfxState.getCOSObject().setItem(COSName.BM,
+            COSName.MULTIPLY); // pdExtGfxState.setBlendMode(BlendMode.MULTIPLY) doesn't work yet, maybe in later version
+        pdExtGfxState.setNonStrokingAlphaConstant(1f);
+        contentStream.setGraphicsStateParameters(pdExtGfxState);
+
+        // contentStream.drawImage(ximage, 20, 20 );
+        // better method inspired by http://stackoverflow.com/a/22318681/535646
+        // reduce this value if the image is too large
+
+        float scale = 1.f;
+        contentStream
+            .drawImage(pdImage, 20, 20, pdImage.getWidth() * scale, pdImage.getHeight() * scale);
+
+        contentStream.restoreGraphicsState();
+      }
+      doc.save(outputPdf);
+    }
+  }
+
+  public static BufferedImage convertSVGToPNG(String url) throws TranscoderException, IOException {
+    ByteArrayOutputStream resultByteStream = new ByteArrayOutputStream();
+
+    TranscoderInput transcoderInput = new TranscoderInput(url);
+    TranscoderOutput transcoderOutput = new TranscoderOutput(resultByteStream);
+
+    PNGTranscoder pngTranscoder = new PNGTranscoder();
+    pngTranscoder.addTranscodingHint(SVGAbstractTranscoder.KEY_HEIGHT, 256f);
+    pngTranscoder.addTranscodingHint(SVGAbstractTranscoder.KEY_WIDTH, 256f);
+    pngTranscoder.transcode(transcoderInput, transcoderOutput);
+
+    resultByteStream.flush();
+
+    return ImageIO.read(new ByteArrayInputStream(resultByteStream.toByteArray()));
+  }
+
+  public static PDImageXObject createImageObjectFromSvg(String name, PDDocument doc)
+      throws IOException, TranscoderException {
+    int dot = name.lastIndexOf('.');
+    if (dot == -1) {
+      throw new IllegalArgumentException("Image type not supported: " + name);
+    }
+    String ext = name.substring(dot + 1).toLowerCase();
+    if (!"svg".equals(ext)) {
+      throw new IllegalArgumentException("Image type not supported: " + name);
+    }
+
+    BufferedImage bim = convertSVGToPNG(name);
+    return LosslessFactory.createFromImage(doc, bim);
+  }
+}
